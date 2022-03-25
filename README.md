5e8d4f1457e17253e4d01622c4b67d83ab9f1cc2
Guillermo Estrada <phrzn@live.com>
2022-03-25 15:36:35 -0600

    List, Put, Get, Delete, working.

--- README.md
+++ README.md
@@ -2,14 +2,57 @@
 
 **Work In Progress**
 
-A pure Go implementation of the [BitCask](https://riak.com/assets/bitcask-intro.pdf) model with no dependecies (hopefully). Meant as an academic exercise to be highly documented and code clarity over optimizations when needed.
+A pure Go implementation of the [BitCask](https://riak.com/assets/bitcask-intro.pdf) model with no dependecies (hopefully).
 
-# Introduction
+It is NOT meant to be a 1:1 replacement for Bitcask as [Riak](https://riak.com/) uses it, nor it is meant to be fully compatible with original Erlang's Bitcask database files.
+
+It IS meant to be an idiomatic Go implementation of the Log-Structured Merge (LSM) Trees with a Write Ahead Log (WAL) model that Bitcask uses. It IS meant to be performant and easy to read through the code, and will implement AT LEAST all the basic features described on [BitCask's model design paper](https://riak.com/assets/bitcask-intro.pdf).
+
+## Introduction
 
 > Bitcask is an Erlang application that provides an API for storing and retrieving key/value data into a log-structured hash table.
 
+It is a fast persistent key/value store with predictable performance and strong fault tolerance, features as per the design document:
+
+- low latency per item read or written
+- high throughput, especially when writing an incoming stream of random items
+- ability to handle datasets much larger than RAM w/o degradation
+- crash friendliness, both in terms of fast recovery and not losing data
+- ease of backup and restore
+- a relatively simple, understandable (and thus supportable) code structure and data format
+- predictable behavior under heavy access load or large volume
+
 You can find the original Bitcask Erlang implementation at: https://github.com/basho/bitcask
 
-# API
+## Roadmap
 
-TODO
+- [x] Create new database
+- [ ] Open existing database
+- [ ] Verify lock for opening existing database
+- [x] List, Put, Get, Delete keys and values
+- [ ] Default sane options and config
+- [ ] Flushable write buffer and Sync configuration
+- [ ] Search, Scan, Prefix, Indexing (Radix tree?)
+- [ ] Merge and compact database files
+- [ ] Create hint files for fat load times
+- [ ] Global time expiration of keys
+- [ ] Extensive unit testing and benchmarks
+- [ ] More...
+
+## API
+
+```go
+
+func bitcask.New(directory string, options bitcask.Opts) (*bitcask.Bitcask, error)
+
+func (bc *Bitcask) Put(key, value []byte) error
+
+func (bc *Bitcask) Get(key []byte) ([]byte, error)
+
+func (bc *Bitcask) Delete(key []byte) error
+
+```
+
+## Benchmarks
+
+Soon...
--- bitcask.go
+++ bitcask.go
@@ -1,51 +1,165 @@
 // Package bitcask ...
 package bitcask
 
+import (
+	"encoding/binary"
+	"fmt"
+	"hash/crc32"
+	"os"
+	"path"
+	"sync"
+	"time"
+)
+
 const (
+	LOCKFILE  = ".lock"
 	TOMBSTONE = "bitcask_tombstone"
 )
 
 // Opts
 type Opts struct {
-	ReadOnly    bool
-	SyncOnPut   bool
-	MaxFileSize int
+	ReadOnly     bool
+	SyncOnPut    bool
+	MaxKeySize   int
+	MaxFileSize  int
+	MaxValueSize int
 }
 
-// Default ...
+// Default options
 var Default = Opts{}
 
 // Bitcask ...
 type Bitcask struct {
+	sync.RWMutex
+	cursor  int
+	current *os.File
+	keydir  map[string]lookup
+	dirname string
 }
 
-// Open ...
+type lookup struct {
+	file      *os.File
+	size      int
+	position  int64
+	timestamp time.Time
+}
+
+// Open reads the files at directory and parses them or creates
+// a new database. Directory has to exist and be writeable by
+// the current process.
 func Open(dirname string, opts Opts) (*Bitcask, error) {
-	return nil, nil
+	if _, err := os.Stat(dirname); os.IsNotExist(err) {
+		return nil, err
+	}
+
+	filename := time.Now().Format("20060102130405.cask")
+	f, err := os.OpenFile(path.Join(dirname, filename), os.O_APPEND|os.O_CREATE, 0600)
+	if err != nil {
+		return nil, err
+	}
+	bc := &Bitcask{}
+	bc.current = f
+	bc.keydir = make(map[string]lookup)
+	bc.dirname = dirname
+
+	err = bc.lockfile()
+	if err != nil {
+		return nil, err
+	}
+
+	return bc, nil
 }
 
-// Get ...
+// Get retrieves the value for a given key fromt he store.
+// If a key does not exist, both value and error will be nil.
 func (bc *Bitcask) Get(key []byte) ([]byte, error) {
-	return nil, nil
+	value, exist := bc.keydir[string(key)]
+	if !exist {
+		return nil, nil
+	}
+
+	bc.RLock()
+	defer bc.RUnlock()
+
+	data := make([]byte, value.size)
+	_, err := value.file.ReadAt(data, value.position)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
 
-// Put ...
+// Put saves the new key/value pair and syncs the store
 func (bc *Bitcask) Put(key, value []byte) error {
+	now := time.Now()
+	data := bc.block(key, value, now)
+
+	bc.Lock()
+	defer bc.Unlock()
+
+	n, err := bc.current.Write(data)
+	if err != nil {
+		return err
+	}
+
+	bc.keydir[string(key)] = lookup{
+		file:      bc.current,
+		size:      len(value),
+		position:  int64(bc.cursor + 16 + len(key)),
+		timestamp: now,
+	}
+
+	bc.cursor += n
 	return nil
 }
 
-// Delete ...
+// Delete removes the key / value from the store
 func (bc *Bitcask) Delete(key []byte) error {
+	data := bc.block(key, []byte(TOMBSTONE), time.Now())
+
+	bc.Lock()
+	defer bc.Unlock()
+
+	n, err := bc.current.Write(data)
+	if err != nil {
+		return err
+	}
+
+	delete(bc.keydir, string(key))
+	bc.cursor += n
 	return nil
 }
 
-// List ...
-func (bc *Bitcask) List() ([][]byte, error) {
-	return nil, nil
+// List returns all the keys in the store as a slice.
+func (bc *Bitcask) List() []string {
+	bc.RLock()
+	defer bc.RUnlock()
+
+	keys := make([]string, 0, len(bc.keydir))
+	for k := range bc.keydir {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
 // Fold ...
-func (bc *Bitcask) Fold(func([]byte, []byte) error) error {
+func (bc *Bitcask) Fold(fn func([]byte, []byte) error) error {
+	bc.RLock()
+	defer bc.RUnlock()
+
+	for key, value := range bc.keydir {
+		data := make([]byte, value.size)
+		_, err := value.file.ReadAt(data, value.position)
+		if err != nil {
+			return err
+		}
+
+		err = fn([]byte(key), data)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -62,5 +176,32 @@ func (bc *Bitcask) Sync() error {
 
 // Close ...
 func (bc *Bitcask) Close() error {
-	return nil
+	return os.Remove(path.Join(bc.dirname, LOCKFILE))
+}
+
+func (bc *Bitcask) block(key, val []byte, timestamp time.Time) []byte {
+	tst := uint32(timestamp.Unix())
+	ksz := uint32(len(key))
+	vsz := uint32(len(val))
+	block := make([]byte, 16, 16+ksz+vsz)
+	binary.BigEndian.PutUint32(block[4:], tst)
+	binary.BigEndian.PutUint32(block[8:], ksz)
+	binary.BigEndian.PutUint32(block[12:], vsz)
+	block = append(block, key...)
+	block = append(block, val...)
+	crc := crc32.ChecksumIEEE(block[4:])
+	binary.BigEndian.PutUint32(block[:], crc)
+	return block
+}
+
+func (bc *Bitcask) lockfile() error {
+	fi, err := bc.current.Stat()
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(
+		path.Join(bc.dirname, LOCKFILE),
+		[]byte(fmt.Sprintf("%d:%s", os.Getpid(), fi.Name())),
+		os.ModePerm,
+	)
 }
